f96776825bb7e950d7fe8d9fbaf3368561946ae1
RODRICKS CHRISTIAN <rodricks111@hotmail.com>
2017-05-15 12:56:27 -0400

    Add files via upload

---
+++ ChatMessage.java
@@ -0,0 +1,31 @@
+import java.io.*;
+/*
+  This class defines the different type of messages that will be exchanged between the
+  Clients and the Server. 
+ */
+public class ChatMessage implements Serializable {
+
+	protected static final long serialVersionUID = 1112122200L;
+
+	// The different types of message sent by the Client
+	// WHOISIN to receive the list of the users connected
+	// MESSAGE an ordinary message
+	// LOGOUT to disconnect from the Server
+	static final int Users = 0, MESSAGE = 1, LOGOUT = 2;
+	private int type;
+	private String message;
+	
+	// constructor
+	ChatMessage(int type, String message) {
+		this.type = type;
+		this.message = message;
+	}
+	
+	// getters
+	int getType() {
+		return type;
+	}
+	String getMessage() {
+		return message;
+	}
+}
